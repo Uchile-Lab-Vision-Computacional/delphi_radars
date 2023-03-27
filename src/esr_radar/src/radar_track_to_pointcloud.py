2e4a35deb071f7d7d1b24fce2cf1baefc2486f44
edu <eduardo.jorquera-int@zippedi.com>
2023-03-27 12:54:03 -0300

    created ROS workspace and package for use in noetic

--- src/esr_radar/src/radar_track_to_pointcloud.py
+++ src/esr_radar/src/radar_track_to_pointcloud.py
@@ -4,13 +4,13 @@ import rospy
 import roslib
 from std_msgs.msg import Header
 from sensor_msgs.msg import PointCloud
-from geometry_msgs.msg import Polygon, PolygonStamped, Point32
-from radar_msgs.msg import RadarTrack, RadarTrackArray
+from geometry_msgs.msg import Point
+from radar_msgs.msg import RadarTrack, RadarTracks
 
-class polygon_transformer():
+class track_transformer():
     def __init__(self):
-        self.sub = rospy.Subscriber("/as_tx/radar_tracks", RadarTrackArray, self.callback)
-        self.pub = rospy.Publisher("/processed/polygon_stamped", PointCloud, queue_size=10)
+        self.sub = rospy.Subscriber("/esr_1/radar_tracks", RadarTracks, self.callback)
+        self.pub = rospy.Publisher("/esr_1/radar_pointcloud", PointCloud, queue_size=10)
         self.header = Header()
         self.header.frame_id = 'esr_1'
 
@@ -21,20 +21,18 @@ class polygon_transformer():
         out_msg.header = self.header
 
         for track in msg.tracks:
-            polygon = track.track_shape 
-            track_points = polygon.points
-            for point in track_points:
-                print('point: ' + str(point))
-                points.append(point)
+            point = track.position
+            #print(point)
+            points.append(point)
         
         out_msg.points = points    
         self.pub.publish(out_msg) 
 
 def main():
     print('initializing node')
-    rospy.init_node("polygon_transformer", anonymous=True)
-    print('initializing detector object')
-    transformer = polygon_transformer()
+    rospy.init_node("track_transformer", anonymous=True)
+    print('initializing transformer object')
+    transformer = track_transformer()
     print('success in initialization')
     try:
         rospy.spin()
@@ -43,4 +41,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
